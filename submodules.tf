74cf912897554f32ed8ad2734fdf67c2f861a41b
Condoriano <15719920+jd4883@users.noreply.github.com>
2021-12-15 07:47:08 +0000

    Update submodules.tf

--- submodules.tf
+++ submodules.tf
@@ -7,8 +7,32 @@ module "external-dns" {
 
 module "internal-dns" {
   count      = var.internal_dns ? 1 : 0
+  providers = {
+    pihole.ns1 = pihole.ns1
+    pihole.ns2 = pihole.ns2
+   }
   domain     = var.domain
   private_ip = var.private_ip
   records    = local.cnames
   source     = "jd4883/dns-bind9/hashicorp"
+}
+  
+ 
+module "ns1" {
+  count         = var.internal_dns ? 1 : 0
+  source        = "jd4883/internal-dns/pihole"
+  providers     = { pihole = pihole.ns1 }
+  parent_domain = var.domain
+  a_records     = {}
+  cnames        = local.cnames
+}
+
+module "ns2" {
+  source        = "jd4883/internal-dns/pihole"
+  count         = var.internal_dns ? 1 : 0
+  providers     = { pihole = pihole.ns2 }
+  parent_domain = var.domain
+  a_records     = {}
+  cnames        = local.cnames
+  depends_on    = [module.ns1]
 }
